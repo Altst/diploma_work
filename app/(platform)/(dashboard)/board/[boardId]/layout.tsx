12d538145205c16696d5517aa34429f3824ac1eb
Altst <94057001+Altst@users.noreply.github.com>
2024-06-04 13:58:30 +0300

    commit

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,68 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { BoardNavbar } from "./_components/board-navbar";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string };
+}) {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || "Board",
+  };
+}
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div className="relative w-full h-full">
+      <img
+        className="absolute inset-0 w-full h-full object-cover blur-sm brightness-50"
+        src="/background-image.jpg"
+        alt="Background"
+      />
+      <BoardNavbar data={board} />
+      <main className="relative pt-28 h-full z-10">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,13 +1,22 @@
+import { auth } from "@clerk/nextjs/server";
 import { OrgControl } from "./_components/org-control";
+import { startCase } from "lodash";
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-      <>
-      <OrgControl/>
-        {children}
-      </>
-    );
-  };
-  
-  export default OrganizationLayout;
-  
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
+
+export default OrganizationLayout;
