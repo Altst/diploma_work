a55a665b6dcec1f730339d723ad4cb045291d568
Altst <94057001+Altst@users.noreply.github.com>
2024-06-07 20:05:00 +0300

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,33 @@ const nextConfig = {
       },
     ],
   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Путь для применения правил CORS
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://stud-task-topaz.vercel.app", // Или укажите конкретный домен, например, 'https://example.com'
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
